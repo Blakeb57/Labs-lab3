7b03c71d3387871be50ea05576ed011bd1f41971
Blake Boehringer <bb148917@ohio.edu>
2020-01-31 17:49:22 -0500

    done

--- prog1.cc
+++ prog1.cc
@@ -1,13 +1,31 @@
 // Don't forget to include the header block
 // more information on keyword const on pages 95 - 97
 // For if else format look at page 77
+/*
+* /home/cs2400/lab3/prog1.cc
+* lab 105
+* Blake Boehringer
+* 9-12-19
+* converting between Celsius and Fahrenheit and vice versa, then displaying the answer
+*/
 
 #include<iostream> 
 #include<cstdlib>
 using namespace std;
-const int INT1 = 15;const int INT2 = 20; int main()
-{cout<<"Sum of"<<INT1 <<"and"<<INT2<<"is"<<INT1+INT2;cout
-<<"Product is"<<INT1*INT2<<endl;
-if(INT1<INT2) cout<<INT2 <<"is bigger";
-else cout<<INT1<<"is bigger"; return (EXIT_SUCCESS);
+
+int main()
+{
+    const int INT1 = 15;
+    const int INT2 = 20; 
+    
+    cout << "Sum of " << INT1 << " and " << INT2 << " is " << INT1+INT2 << endl;
+    cout << "Product is " << INT1*INT2 << endl;
+
+    if(INT1<INT2){
+        cout << INT2 << " is bigger" << endl;;
+    }else{
+        cout<< INT1 << " is bigger" << endl;
+    }
+
+    return (EXIT_SUCCESS);
 }
